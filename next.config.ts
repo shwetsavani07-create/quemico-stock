70b9fd398e4b6b21cae511d19b01a1fddb94211d
shwetsavani07-create <shwetsavani07@gmail.com>
2026-08-20 18:50:03 +0530

    frist

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.1.65"],
+
   serverExternalPackages: ["mongoose"],
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
+
   experimental: {
     serverActions: {
       bodySizeLimit: "5mb",
     },
     proxyClientMaxBodySize: "5mb",
   },
+
   headers: async () => [
     {
       source: "/sw.js",
