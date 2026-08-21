0f601a3954669e8445f1c15a71215b65e406da07
shwetsavani07-create <shwetsavani07@gmail.com>
2026-08-21 15:26:15 +0530

    frist

--- next.config.ts
+++ next.config.ts
@@ -16,7 +16,7 @@ const nextConfig: NextConfig = {
 
   experimental: {
     serverActions: {
-      bodySizeLimit: "5mb",
+      bodySizeLimit: "10mb",
     },
     proxyClientMaxBodySize: "5mb",
   },
